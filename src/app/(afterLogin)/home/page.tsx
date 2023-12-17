4a50ab39f495fc76c7a2539f45cea4a551d03280
한태경 <hanskd12345@gmail.com>
2023-12-17 15:04:45 +0900

    Feat: 홈탭 레이아웃

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import style from './home.module.css';
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
 
-function page() {
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+import TabProvider from './_component/TabProvider';
+
+export default function Home() {
   return (
-    <div>home page</div>
+    <main className={style.main}>
+    <TabProvider>
+    <Tab/>
+        <PostForm />
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+    </TabProvider>
+    </main>
   )
 }
-
-export default page
