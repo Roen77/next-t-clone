593132428351a23f0f53a0508fd1175d896848be
한태경 <hanskd12345@gmail.com>
2023-12-17 16:18:56 +0900

    Feat: explore 페이지 레이아웃 작업

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import style from "./explore.module.css";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
 
-function Explore() {
+export default function Home() {
   return (
-    <div>Explore</div>
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
   )
 }
-
-export default Explore
