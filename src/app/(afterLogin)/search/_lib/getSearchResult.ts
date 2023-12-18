31040cbe6f156ce22eae150e1302cd3e4915be37
한태경 <hanskd12345@gmail.com>
2023-12-19 02:09:55 +0900

    Feat: 검색 파라미터 변경 작업

--- src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -4,15 +4,16 @@ import {Post} from "@/model/Post";
 export const getSearchResult: QueryFunction<Post[], [_1: string, _2: string, searchParams: { q: string, pf?: string, f?: string }]>
   = async ({ queryKey }) => {
   const [_1, _2, searchParams] = queryKey;
-  const urlSearchParams = new URLSearchParams(searchParams);
+  // const urlSearchParams = new URLSearchParams(searchParams);
   // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/search/${urlSearchParams.toString()}`, {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/search/${searchParams.q}?${searchParams.toString()}`, {
-    next: {
-      tags: ['posts', 'search', searchParams.q],
-    },
-    credentials: 'include',
-    cache: 'no-store',
-  });
+    const urlSearchParams = new URLSearchParams(searchParams);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?${urlSearchParams.toString()}`, {
+      next: {
+        tags: ['posts', 'search', searchParams.q],
+      },
+      credentials: 'include',
+      cache: 'no-store',
+    });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
