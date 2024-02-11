7d53a2b6127cebda9b719f21f1a63c1108779070
한태경 <hanskd12345@gmail.com>
2024-02-11 14:00:43 +0900

    Feat: 답글 기능 추가

--- src/app/(afterLogin)/_component/PostArticle.tsx
+++ src/app/(afterLogin)/_component/PostArticle.tsx
@@ -1,37 +1,44 @@
 "use client";
 
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import style from './post.module.css';
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Post } from "@/model/Post";
+
+// type Props = {
+//   children: ReactNode,
+//   post: {
+//     postId: number;
+//     content: string,
+//     User: {
+//       id: string,
+//       nickname: string,
+//       image: string,
+//     },
+//     createdAt: Date,
+//     Images: any[],
+//   }
+// }
 
 type Props = {
   children: ReactNode,
-  post: {
-    postId: number;
-    content: string,
-    User: {
-      id: string,
-      nickname: string,
-      image: string,
-    },
-    createdAt: Date,
-    Images: any[],
-  }
+  post: Post
 }
 
-export default function PostArticle({ children, post}: Props) {
+
+export default function PostArticle({ children, post }: Props) {
   const router = useRouter();
   let target = post;
-  // if (post.Original) {
-  //   target = post.Original;
-  // }
+  if (post.Original) {
+    target = post.Original;
+  }
   const onClick = () => {
     router.push(`/${target.User.id}/status/${target.postId}`);
   }
 
   return (
     <article onClick={onClick} className={style.post}>
-    {/* <article onClickCapture={onClick} className={style.post}> */}
+      {/* <article onClickCapture={onClick} className={style.post}> */}
       {children}
     </article>
   );
