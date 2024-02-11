cac925c03d7fac5648a38a07594d0c4c7ce27d50
한태경 <hanskd12345@gmail.com>
2024-02-11 12:28:18 +0900

    Feat: 메타데이터 설정 추가

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -7,26 +7,36 @@ import { getUserServer } from './_lib/getUserServer';
 import { getUserPosts } from './_lib/getUserPosts';
 import UserPosts from './_component/UserPosts';
 import UserInfo from './_component/UserInfo';
+import { User } from '@/model/User';
+
+
+export async function generateMetadata({ params }: Props) {
+  const user: User = await getUserServer({ queryKey: ["users", params.username] });
+  return {
+    title: `${user.nickname} (${user.id}) / Z`,
+    description: `${user.nickname} (${user.id}) 프로필`,
+  }
+}
 
 type Props = {
   params: { username: string },
 }
-export default async function Profile({params}: Props) {
-  const {username} = params;
+export default async function Profile({ params }: Props) {
+  const { username } = params;
   const session = await auth();
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({queryKey: ['users', username], queryFn: getUserServer})
-  await queryClient.prefetchQuery({queryKey: ['posts', 'users', username], queryFn: getUserPosts})
+  await queryClient.prefetchQuery({ queryKey: ['users', username], queryFn: getUserServer })
+  await queryClient.prefetchQuery({ queryKey: ['posts', 'users', username], queryFn: getUserPosts })
   const dehydratedState = dehydrate(queryClient);
 
   return (
     <main className={style.main}>
-    <HydrationBoundary state={dehydratedState}>
-      <UserInfo username={username} session={session} />
-      <div>
-        <UserPosts username={username} />
-      </div>
-    </HydrationBoundary>
-  </main>
+      <HydrationBoundary state={dehydratedState}>
+        <UserInfo username={username} session={session} />
+        <div>
+          <UserPosts username={username} />
+        </div>
+      </HydrationBoundary>
+    </main>
   )
 }
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import TrendSection from "./_component/TrendSection";
 import style from "./explore.module.css";
 import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
 
 
+export const metadata: Metadata = {
+  title: '탐색 / Z',
+  description: '탐색해보세요!',
+
+}
 export default function Home() {
   return (
     <main className={style.main}>
@@ -11,7 +17,7 @@ export default function Home() {
       </div>
       <div className={style.trend}>
         <h3>나를 위한 트렌드</h3>
-        <TrendSection/>
+        <TrendSection />
         {/* <Trend />
         <Trend />
         <Trend />
